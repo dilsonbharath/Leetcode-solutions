da2393862fce65764922dcdbc46c1bc2eb3e606c
Dilson Bharath R <dilsonbharath76@gmail.com>
2025-11-05 20:02:09 +0530

    Time: 0 ms (100%), Space: 19.6 MB (17.79%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -7,10 +7,9 @@ class Solution:
             if i not in di:
                 di[i] = 1
             else:
-                di[i] += 1
+                if di[i] == 2:
+                    del di[i]
+                else:
+                    di[i] += 1
 
-        for i,j in di.items():
-            if j==1:
-                return i
-
-        return -1
+        return list(di.keys())[0]
